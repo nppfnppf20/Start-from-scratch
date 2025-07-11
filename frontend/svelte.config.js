8f0b747e85c78092340fb48e74f82eb260d33cce
nppfnppf20 <rogersjoshw@gmail.com>
2025-07-11 09:08:04 +0100

    changed adaptor

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,17 +1,11 @@
-// frontend/svelte.config.js
-import adapter from '@sveltejs/adapter-static'; // Ensure this is adapter-static
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html', // Important for SPAs
-			precompress: false
-		})
+		adapter: adapter()
 	}
 };
 
